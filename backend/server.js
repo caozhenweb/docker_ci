411dccefa359d355afcbf740b3c1e993ab17d01c
web_caozhen <543472527@qq.com>
2020-09-23 20:30:21 +0800

    test push

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ app.get('/api/top', async (req, res) => {
     return b.solded - a.solded
   })
   res.json({
-    code: 0, 
+    code: 0,
 
-    data: newData.slice(0, 5)
+    data: newData.slice(0, 2)
   })
 })
 
